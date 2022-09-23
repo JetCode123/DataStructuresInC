3741e9746bf0fbbd7315eebe827000f28208b1d0
Lu <786854559@qq.com>
2022-09-23 22:26:54 +0800

    Add files via upload

---
+++ josephusproblem.c
@@ -0,0 +1,20 @@
+/**
+ * @file JosephuProblem.c
+ * @author {Robin Lu}
+ * @brief '约瑟夫问题:' also named as ''丢手帕问题:''
+ * @version 1.0
+ * @date: 2022.8.29 周1 
+ * 
+ * @copyright Copyright (c) 2022 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    return 0;
+}
